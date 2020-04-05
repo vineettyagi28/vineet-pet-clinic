a9703907456f7049e124f13b8d5c5e32d844ddb7
vineettyagi28@gmail.com <vineettyagi28@gmail.com>
2020-04-05 13:34:55 +0530

    Initial Project Setup

---
+++ src/main/java/com/thinksmallgroup/vineetpetclinic/VineetPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.thinksmallgroup.vineetpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VineetPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VineetPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thinksmallgroup/vineetpetclinic/VineetPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinksmallgroup.vineetpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VineetPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
