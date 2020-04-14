0e7e27be3b14595c8211a71c6f278d1dddccbf7d
vineettyagi28@gmail.com <vineettyagi28@gmail.com>
2020-04-14 17:45:44 +0530

    Added Owner Index Page and Owner Controller. closes #14

---
+++ pet-clinic-web/src/main/java/com/thinksmallgroup/vineetpetclinic/controllers/OwnerController.java
@@ -0,0 +1,19 @@
+package com.thinksmallgroup.vineetpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+
+}
