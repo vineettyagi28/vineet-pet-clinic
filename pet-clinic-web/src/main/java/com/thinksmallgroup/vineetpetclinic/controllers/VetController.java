4b52a6d557c33a48546d60c99ab724947926e8ef
vineettyagi28@gmail.com <vineettyagi28@gmail.com>
2020-04-14 17:37:05 +0530

    Added Vet Index Page and IndexController. closes #13

---
+++ pet-clinic-web/src/main/java/com/thinksmallgroup/vineetpetclinic/controllers/VetController.java
@@ -0,0 +1,18 @@
+package com.thinksmallgroup.vineetpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+
+}
